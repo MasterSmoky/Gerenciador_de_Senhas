b458b338f3f4c960fadce81fc7736ffaf11cebba
Luiz Henrique <oni.master.232084@outlook.com>
2023-11-09 15:49:18 -0300

    criador de senha

---
+++ main.py
@@ -0,0 +1,25 @@
+# Uso da lib "random" para randomizar a senha
+import random
+
+# Variaveis de caracteres na senha
+
+Maiu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Minu = "abcdefghijklmnopqrstuvwxyz"
+Nume = "0123456789"
+Simb = "!@#$%¨&*-+,."
+
+Senha = Maiu + Minu + Nume + Simb
+
+# Pergunta e funcionamento do código
+while True:
+
+    Tamanho = int(input("Qual o tamanho da senha que deseja criar ? > "))
+
+    if Tamanho < 8 or Tamanho > 32:
+        print("Senha muito pequena ou muito grande, por favor entre com uma senha entre 8 a 32 caracteres")
+    else:
+        criacao_Senha = ''.join(random.sample(Senha,Tamanho))
+        print("Senha criada:", criacao_Senha) 
+        break
+
+
