f46c1c9ded8ed5b35e3379944957da30b9925092
Luiz Henrique <oni.master.232084@outlook.com>
2023-11-10 09:16:50 -0300

    Adição de salvamento em .txt

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 Maiu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 Minu = "abcdefghijklmnopqrstuvwxyz"
 Nume = "0123456789"
-Simb = "!@#$%¨&*-+,."
+Simb = "!@#$%&*-+,."
 
 Senha = Maiu + Minu + Nume + Simb
 
@@ -16,10 +16,15 @@ while True:
     Tamanho = int(input("Qual o tamanho da senha que deseja criar ? > "))
 
     if Tamanho < 8 or Tamanho > 32:
+
         print("Senha muito pequena ou muito grande, por favor entre com uma senha entre 8 a 32 caracteres")
+        
     else:
+
         criacao_Senha = ''.join(random.sample(Senha,Tamanho))
         print("Senha criada:", criacao_Senha) 
-        break
 
+        with open('senhas.txt', 'a') as arquivo:
+            arquivo.write(criacao_Senha + '\n')
 
+    break
